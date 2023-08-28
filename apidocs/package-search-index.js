490a1c983587d90e5196aefe94dc4b53376854f7
QubitPi <jack20220723@gmail.com>
2023-08-28 07:28:12 +0000

    deploy: d8aa5d275a045288e3b19bc6e2968e212584b7c1

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.paiondata.astraios.application"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.paiondata.astraios.application"},{"l":"com.paiondata.astraios.config"},{"l":"com.paiondata.astraios.web.filters"}];updateSearchResults();
