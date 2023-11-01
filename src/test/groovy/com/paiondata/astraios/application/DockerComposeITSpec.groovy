aeefde82c50a45786903d7a283fccf2bd5a66473
Jack <jack20220723@gmail.com>
2023-11-01 18:31:08 +0800

    Fix deployment error

--- src/test/groovy/com/paiondata/astraios/application/DockerComposeITSpec.groovy
+++ src/test/groovy/com/paiondata/astraios/application/DockerComposeITSpec.groovy
@@ -32,7 +32,7 @@ class DockerComposeITSpec extends AbstractITSpec {
             .withExposedService(
                     "web",
                     WS_PORT,
-                    Wait.forHttp("/v1/data/note").forStatusCode(200)
+                    Wait.forHttp("/v1/data/graph").forStatusCode(200)
             )
 
     @Override
@@ -50,7 +50,7 @@ class DockerComposeITSpec extends AbstractITSpec {
         RestAssured
                 .given()
                 .when()
-                .get("note")
+                .get("graph")
                 .then()
                 .statusCode(200
                 )
